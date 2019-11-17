82ae57bdfa26956da86b38cb66b789f7900f1a38
Rogério Barreto <rbmdesenvolvimento@gmail.com>
2019-11-17 02:16:51 -0300

    Criando a Tela de Login personalizada

--- djangoum3/settings.py
+++ djangoum3/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'usuarios.CustomUsuario'
+
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
