848de17fa1a99fdba7fe3af8e4de67bb514c9878
Rogério Barreto <rbmdesenvolvimento@gmail.com>
2019-11-17 00:05:57 -0300

    Atualizando migrations, forms.py e models.py

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -4,15 +4,16 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUsuarioCreateForm, CustomUsuarioChangeForm
 from .models import CustomUsuario
 
+
 @admin.register(CustomUsuario)
 class CustomUsuarioAdmin(UserAdmin):
     add_form = CustomUsuarioCreateForm
-    formm = CustomUsuarioChangeForm
+    form = CustomUsuarioChangeForm
     model = CustomUsuario
-    list_display = ('first_name', 'last_name', 'email', 'fone', 'cpf', 'is_staff')
-    fildsets = (
+    list_display = ('first_name', 'last_name', 'email', 'fone', 'is_staff')
+    fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Informações Pessoais', {'fields': ('first_name', 'last_name', 'fone', 'cpf')}),
-        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permisssions')}),
-        ('Datas importantes', {'fields': ('last_login', 'date_joined')}),
+        ('Informações Pessoais', {'fields': ('first_name', 'last_name', 'fone')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
     )
--- usuarios/forms.py
+++ usuarios/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUsuario
 
+
 class CustomUsuarioCreateForm(UserCreationForm):
 
     class Meta:
@@ -10,16 +11,16 @@ class CustomUsuarioCreateForm(UserCreationForm):
         labels = {'username': 'Username/E-mail'}
 
     def save(self, commit=True):
-        user = super().save(commit=True)
+        user = super().save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         user.email = self.cleaned_data["username"]
         if commit:
             user.save()
-            return user
+        return user
+
 
 class CustomUsuarioChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUsuario
-        fields = ['first_name', 'last_name', 'fone', 'cpf']
-
+        fields = ('first_name', 'last_name', 'fone', 'cpf')
